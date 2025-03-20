b6a09de36515372632b2658fc9a4ab963070287e
Артур Мыльников <kuraidatenshidesu@gmail.com>
2025-03-20 15:49:50 +0300

    Added alarm sound and made template for timer settings

--- src/components/Timer/ui/Timer.tsx
+++ src/components/Timer/ui/Timer.tsx
@@ -6,17 +6,22 @@ type TimerStatus = "working" | "resting";
 
 const Timer: FC = () => {
   const [isTimerActive, setIsTimerActive] = useState(false);
+  const [workTime, setWorkTime] = useState(25);
+  const [restTime, setRestTime] = useState(5);
   const [time, setTime] = useState(0);
   const [timerStatus, setTimerStatus] = useState<TimerStatus>("working");
+  const audio = new Audio("/public/sound.wav");
 
   useEffect(() => {
-    if (timerStatus === "working" && time === 5) {
+    if (timerStatus === "working" && time === workTime * 60) {
       setTimerStatus("resting");
+      audio.play();
       setTime(0);
     }
 
-    if (timerStatus === "resting" && time === 5) {
+    if (timerStatus === "resting" && time === restTime * 60) {
       setTimerStatus("working");
+      audio.play();
       setTime(0);
     }
   }, [time]);
@@ -39,6 +44,11 @@ const Timer: FC = () => {
 
   return (
     <div className={style.Timer}>
+      <input type="text" value={workTime} onChange={(e) => setWorkTime(parseInt(e.target.value))} />
+      <input type="text" value={restTime} onChange={(e) => setRestTime(parseInt(e.target.value))} />
+      <button className={[style.Button, style.Setting].join(" ")}>
+        <span className="material-symbols-outlined">settings</span>
+      </button>
       <h1 className={style.Title}>Pomodoro timer</h1>
       <p className={style.Time}>{getTimestring(time)}</p>
       <p className={style.Status}>{timerStatus === "working" ? "Time for work" : "Have a little break"}</p>
